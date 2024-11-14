cf2a79e328990df98268f6694b79f4b27e3f7d51
Max-MIT <maksudbek198501@gmail.com>
2024-11-14 21:53:21 +0900

    added

---
+++ server.js
@@ -0,0 +1,24 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 2 Session code (хозирча бизни мисолимизда йук)
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.get("/hello",function (req, res) {
+    res.end(`<h1>HELLO WORLD</h1>`);
+});
+
+const server = http.createServer();
+let PORT = 3000;
+server.listen(PORT, function() {
+ console.log(`The server is running successfully on port: ${PORT}`);
+}); 
