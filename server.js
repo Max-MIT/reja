eafc692565cf7d6214f1982005ff6fbbb6579e2a
Max-MIT <maksudbek198501@gmail.com>
2024-11-16 20:16:28 +0900

    feat: devolop author router page

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // 2 Session code (хозирча бизни мисолимизда йук)
 // 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello",function (req, res) {
-    res.end(`<h1>HELLO WORLD</h1>`);
+app.post("/create-item",(req, res) => {
+    console.log(req.body);
+    res.json({test: "success" });
 });
 
-const server = http.createServer();
+app.get('/author', (rec, res) => {
+ res.render("author", );
+})
+
+app.get("/", function (req, res) {
+    res.render("harid");
+});
+
+const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function() {
+server.listen(PORT, function () {
  console.log(`The server is running successfully on port: ${PORT}`);
 }); 
