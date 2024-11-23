cf0a80c2f0579392fca1925d31b3d50d856a73d9
Max-MIT <maksudbek198501@gmail.com>
2024-11-23 15:49:58 +0900

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const http = require("http");
-const app = require("./app");
 
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
- console.log(`The server is running successfully on port: ${PORT}`);
+const mongodb = require("mongodb");
 
-}); 
+let db;
+const connectionString =
+ "mongodb+srv://maksudbek:Wellness1985@cluster0.hnxci.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+
+mongodb.connect(
+    connectionString, 
+    {
+    useNewUrlParser: true,
+     useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) console.log("ERROR on connection MongoTimur");
+        else {
+          console.log("MongoDB connection succeed Timur");
+          module.exports = client;
+          
+          const app = require("./app");
+          const server = http.createServer(app);
+          let PORT = 3000;
+          server.listen(PORT, function () {
+            console.log(
+                `The server is running successfully on port: ${PORT},`
+              );
+            }); 
+        }
+    });
+
