10195d06b050e1c27ef5077f31d1a3b982db3b1c
Max-MIT <maksudbek198501@gmail.com>
2024-11-23 22:05:25 +0900

    feat: CRUD operatorsiyalari

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
-// MangooDB connect
+// MangooDB choqirish
+const db = require("./server").db();
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -16,12 +17,31 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.post("/create-item",(req, res) => {
-    // TODO: code with db here
+app.post("/create-item", (req, res) => {
+    console.log('user entered /create-item');
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
 });
 
 app.get("/", function (req, res) {
-    res.render("reja");
+    console.log('user entered /');
+   db.collection("plans")
+   .find()
+   .toArray((err, data) => {
+    if (err) {
+        console.log(err);
+        res.end("something went wrong");
+    } else {
+        res.render("reja", { items: data });
+    }
+   });
 });
 
 module.exports = app;
